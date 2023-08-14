47073777a2324103016e2bf6ac562ce3af9a724f
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-08-14 19:59:05 +0100

    .

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ Users should be able to:
 ### Links
 
 - Solution URL: [Add solution URL here](https://your-solution-url.com)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Live Site URL: [Add live site URL here](https://newsign.netlify.app)
 
 
 ### Built with
@@ -39,6 +39,6 @@ The major things I learnt were working with states in React, and how to handle f
 
 ## Author
 
-- Website - [Add your name here](https://www.gentledove.netlify.app)
+- Website - [Add your name here](https://gentledove.netlify.app)
 - Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/teoso)
 - Twitter - [@yourusername](https://www.twitter.com/theGentleDove)
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -7,33 +7,43 @@ import Image from "next/image";
 import "../page";
 import Home from "../page";
 import { useState } from "react";
-import { useSearchParams } from "next/navigation";
+// import { useSearchParams } from "next/navigation";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+
 export default function Success() {
-  const searchParams = useSearchParams();
-  const [success, setSuccess] = useState(false);
+  const router = useRouter();
+  const pathname = usePathname();
+  const query = useSearchParams();
+  // const searchParams = useSearchParams();
+  // const [success, setSuccess] = useState(false);
+  // function backHome() {
+  //   setSuccess(true);
+  //   alert("NB: No actual mail was sent, this is just a demo")
+  // }
   function backHome() {
-    setSuccess(true);
+    // Instead of setting success to true, you can use router to navigate back
+    router.push('https://newsign.netlify.app');
     alert("NB: No actual mail was sent, this is just a demo")
   }
 
   return (
     <>
-      {!success && (
+      {/* {!success && ( */}
         <main className="main2">
           <Image src={check} className="check2" alt="..." />
           <h1 className="h12">Thanks for subscribing</h1>
           <p className="ps">
             A confirmation email has been sent to{" "}
             <strong>
-              <span id="userEmail">{searchParams.get("email")}</span>
+              <span id="userEmail">{query.get("email")}</span>
             </strong>
             . Please open it and click the button inside to confirm your
             subscription.
           </p>
           <button className="btns" onClick={backHome}>Dismiss message</button>
         </main>
-      )}
-      {success && <Home />}
+      {/* )} */}
+      {/* {success && <Home />} */}
     </>
   );
 }
